05c9aa47bc09fcb8f4a48f112004266f6dac0d7c
David McClure <david.mcclure@virginia.edu>
2013-08-15 14:25:31 -0400

    Sketches in shell for well-formatted Text class.

---
+++ Text.py
@@ -0,0 +1,29 @@
+
+# vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80;
+
+# intra.Text: A text on which queries can be performed.
+# :copyright: (c) 2013, David McClure
+
+
+class Text:
+
+    def __init__(self, text):
+
+        '''
+        Store and tokenize the raw text string.
+
+        :type text: string
+        :param text: The raw text string.
+        '''
+
+        pass
+
+    def tokenize(self):
+
+        '''
+        Populate the `tokens` attribute with a list of tuples, one for each
+        word in the text, each containing the word and the original character
+        offset of the word in the text.
+        '''
+
+        pass
