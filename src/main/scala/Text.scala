6578f65396fa8cf395589930c79cfd12201744e6
David McClure <david.mcclure@virginia.edu>
2013-09-27 16:44:54 -0700

    Formatting cleanup in tokenze.

--- src/main/scala/Text.scala
+++ src/main/scala/Text.scala
@@ -4,41 +4,51 @@
 
 package com.mcclure.intra
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
 
 
 class Text(val text: String) {
 
+
   val tokens = ArrayBuffer[Tuple2[String, Int]]()
+  val positions = HashMap[String, ArrayBuffer[Int]]()
+
 
   def tokenize {
 
-    var charOffset = 0
-    var tokenCount = 0
     var token = ""
+    var start = 0
+    var count = 0
 
-    // Walk the characters in the text.
-    for ((char, index) <- text.zipWithIndex) {
+    for ((c, i) <- text.zipWithIndex) {
 
       // If the character is a letter, add it to the current token. If we're
       // at the left boundary of a new token, store the character offset.
 
-      if (char.isLetter) {
-        if (token == "") charOffset = index
-        token += char
+      if (c.isLetter) {
+        if (token == "") start = i
+        token += c
       }
 
       // If a non-empty token has been accumulated and either (a) the current
-      // character isn't a letter, meaning we're at the end of a word, or (b)
-      // we're at the last character in the text, store the token / character
-      // offset and clear the running token.
+      // character isn't a letter, meaning we've reached the end of a word, or
+      // (b) we're at the last character in the text, store the token with its
+      // character offset and clear the running token.
 
-      if (token != "" && (!char.isLetter || index+1 == text.length)) {
-        tokens += Tuple2(token, index)
+      if (token != "" && (!c.isLetter || i+1 == text.length)) {
+
+        // Register the new token.
+        tokens += Tuple2(token, start)
+        positions.getOrElseUpdate(token, ArrayBuffer[Int]()) += count
+
+        count += 1
         token = ""
+
       }
 
     }
 
   }
+
 
 }
