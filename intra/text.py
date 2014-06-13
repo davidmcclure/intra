60eae128727ebf431f7eb14fc8450bf6c7588dc0
David McClure <davidwilliammcclure@gmail.com>
2014-06-13 08:17:24 -0700

    Stubs in the Text class, moves in the strip_tags helper.

--- intra/text.py
+++ intra/text.py
@@ -3,6 +3,7 @@
 class Text(object):
 
     def __init__(self, text):
+
         """
         Store and tokenize the text.
 
@@ -13,6 +14,7 @@ class Text(object):
         self.tokenize()
 
     def tokenize(self):
+
         """
         Tokenize the text. Store left and right character offsets for each
         token, and build a token -> offsets map.
@@ -20,12 +22,5 @@ class Text(object):
 
         self.tokens = []
         self.offsets = {}
-
-        pass # TODO
-
-    def get_bare_tokens(self):
-        """
-        Get a 1-dimensional array of the raw, stemmed tokens.
-        """
 
         pass # TODO
